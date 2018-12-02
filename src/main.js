9cd18e8aef12e1753d42dcfdd943e0cfb15ec6a6
GulajavaMinistudio <gulajava.mini@gmail.com>
2018-12-02 08:31:35 +0700

    Inisialisasi awal aplikasi web Jam Dinding Vue

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
+
+// import data library pihak ketiga
+// untuk mode produksi, gunakan mode CDN
+import 'bootstrap/dist/css/bootstrap.min.css';
+import jQuery from 'jquery/dist/jquery.slim.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min';
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+window.jQuery = jQuery;
+window.$ = jQuery;
 Vue.config.productionTip = false;
 
 new Vue({
